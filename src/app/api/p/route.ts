e00319d78bc7b4a84519c5c4f0f7572a6bd3cdbb
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2024-12-13 16:51:12 +0530

    fix chromium

--- src/app/api/p/route.ts
+++ src/app/api/p/route.ts
@@ -20,7 +20,7 @@ const isDev = process.env.NODE_ENV === "development";
 const userAgent =
   "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Mobile Safari/537.36";
 
-export const maxDuration = 60; // This function can run for a maximum of 60 seconds (update by 2024-05-10)
+export const maxDuration = 60;
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
